57ceca8d91e0b39481bad8f2468bc43cc6413433
ejmdenham <ejmdenham@gmail.com>
2022-02-13 17:39:17 +0100

    Working Onboarding

---
+++ Redline_test/src/screens/Login/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { View, Text, Button } from "react-native";
+
+import styles from "./styles";
+
+const Login = ({navigation}) => {
+    return (
+
+        <View style={styles.container}>
+            <Text > Login Screen </Text>
+            <Button
+            title="Click Here"
+            onPress={() => alert("BING!")}
+            />
+        </View>
+        
+    );
+};
+
+export default Login;
---
+++ Redline_test/src/screens/Login/styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignContent: "center",
+        justifyContent: "center"
+    },
+
+    text: {
+        textAlign: "center"
+    },
+});
+
+export default styles;
